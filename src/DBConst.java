013866b579b5a688ed99f5306eef485426995f66
hcloudskns <seungmin.lim@namutech.co.krgit config --global user.name hcloudskns;git config --global user.email  seungmin.lim@namutech.co.kr>
2021-09-02 16:01:16 +0900

    fix gitignore

--- src/DBConst.java
+++
@@ -1,8 +0,0 @@
-public final class DBConst {
-	
-	public static final String DBConnectionStr ="jdbc:mariadb://192.168.102.110:3306/HCLOUD";
-	public static final String userName = "root";
-	public static final String userPwd = "P@$$w0rd";
-	public static final int policyServerPort = 10001;
-	
-}
